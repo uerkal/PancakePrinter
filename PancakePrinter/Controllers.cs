64374c317af92ee58f4d72c939ab43356aa74f05
Umut Erkal <umute@msn.com>
2016-01-05 16:15:51 +0200

    Initial version

---
+++ PancakePrinter/Controllers.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Devices.Enumeration;
+using Windows.Devices.HumanInterfaceDevice;
+
+namespace PancakePrinter
+{
+    /// <summary>
+    /// **** Controllers Class ****
+    /// HID Controller devices - XBox controller
+    ///   Data transfer helpers: message parsers, direction to motor value translatores, etc.
+    /// </summary>
+    public class Controllers
+    {
+        public static bool FoundLocalControlsWorking = false;
+
+        private static XboxHidController controller;
+        private static int lastControllerCount = 0;
+
+        private static PancakeDrive pancakeDrive = new PancakeDrive();
+
+        public static async void XboxJoystickInit()
+        {
+            string deviceSelector = HidDevice.GetDeviceSelector(0x01, 0x05);
+            DeviceInformationCollection deviceInformationCollection = await DeviceInformation.FindAllAsync(deviceSelector);
+
+            if (deviceInformationCollection.Count == 0)
+            {
+                Debug.WriteLine("No Xbox360 controller found!");
+            }
+            lastControllerCount = deviceInformationCollection.Count;
+
+            foreach (DeviceInformation d in deviceInformationCollection)
+            {
+                Debug.WriteLine("Device ID: " + d.Id);
+
+                HidDevice hidDevice = await HidDevice.FromIdAsync(d.Id, Windows.Storage.FileAccessMode.Read);
+
+                if (hidDevice == null)
+                {
+                    try
+                    {
+                        var deviceAccessStatus = DeviceAccessInformation.CreateFromId(d.Id).CurrentStatus;
+
+                        if (!deviceAccessStatus.Equals(DeviceAccessStatus.Allowed))
+                        {
+                            Debug.WriteLine("DeviceAccess: " + deviceAccessStatus.ToString());
+                            FoundLocalControlsWorking = true;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine("Xbox init - " + e.Message);
+                    }
+
+                    Debug.WriteLine("Failed to connect to the controller!");
+                }
+
+                controller = new XboxHidController(hidDevice);
+                controller.DirectionChanged += Controller_DirectionChanged;
+            }
+        }
+
+        public static async void XboxJoystickCheck()
+        {
+            string deviceSelector = HidDevice.GetDeviceSelector(0x01, 0x05);
+            DeviceInformationCollection deviceInformationCollection = await DeviceInformation.FindAllAsync(deviceSelector);
+            if (deviceInformationCollection.Count != lastControllerCount)
+            {
+                lastControllerCount = deviceInformationCollection.Count;
+                XboxJoystickInit();
+            }
+        }
+
+        private static void Controller_DirectionChanged(ControllerVector sender)
+        {
+            FoundLocalControlsWorking = true;
+            Debug.WriteLine("Direction: " + sender.Direction + ", Magnitude: " + sender.Magnitude);
+            GamepadToPancakePrinter((sender.Magnitude < 2500) ? ControllerDirection.None : sender.Direction, sender.Magnitude);
+        }
+
+        static void GamepadToPancakePrinter(ControllerDirection dir, int magnitude)
+        {
+            switch (dir)
+            {
+                case ControllerDirection.Down:
+                    pancakeDrive.SetY(magnitude * -1);
+                    pancakeDrive.SetX(0);
+                    break;
+                case ControllerDirection.Up:
+                    pancakeDrive.SetY(magnitude);
+                    pancakeDrive.SetX(0);
+                    break;
+                case ControllerDirection.Left:
+                    pancakeDrive.SetX(magnitude * -1);
+                    pancakeDrive.SetY(0);
+                    break;
+                case ControllerDirection.Right:
+                    pancakeDrive.SetX(magnitude);
+                    pancakeDrive.SetY(0);
+                    break;
+                case ControllerDirection.DownLeft:
+                    pancakeDrive.SetX(magnitude * -1);
+                    pancakeDrive.SetY(magnitude * -1);
+                    break;
+                case ControllerDirection.DownRight:
+                    pancakeDrive.SetX(magnitude);
+                    pancakeDrive.SetY(magnitude * -1);
+                    break;
+                case ControllerDirection.UpLeft:
+                    pancakeDrive.SetX(magnitude * -1);
+                    pancakeDrive.SetY(magnitude);
+                    break;
+                case ControllerDirection.UpRight:
+                    pancakeDrive.SetX(magnitude);
+                    pancakeDrive.SetY(magnitude);
+                    break;
+                default:
+                    pancakeDrive.SetX(0);
+                    pancakeDrive.SetY(0);
+                    break;
+            }
+        }
+    }
+}
---
+++ PancakePrinter/PancakeDrive.cs
@@ -0,0 +1,119 @@
+﻿using System.Threading;
+using Windows.Devices.Gpio;
+
+namespace PancakePrinter
+{
+    public class PancakeDrive
+    {
+        // GPIO Pins to motor controllers
+        static GpioPin stepperXPulse;
+        static GpioPin stepperXDir;
+        static GpioPin stepperXEn;
+
+        static GpioPin stepperYPulse;
+        static GpioPin stepperYDir;
+        static GpioPin stepperYEn;
+
+        // GPIO Pin to servo motor
+        static GpioPin servoPulse;
+
+        // X-Y Movement timer
+        static Timer tmrPulse;
+
+        // X-Y Movement switch
+        static bool xMove = false;
+        static bool yMove = false;
+
+        public PancakeDrive()
+        {
+            // Initialize GPIO Controller
+            var gpio = GpioController.GetDefault();
+
+            // Set X motor pins
+            stepperXPulse = GpioController.GetDefault().OpenPin(23);
+            stepperXPulse.SetDriveMode(GpioPinDriveMode.Output);
+            stepperXPulse.Write(GpioPinValue.Low);
+
+            stepperXDir = gpio.OpenPin(24);
+            stepperXDir.SetDriveMode(GpioPinDriveMode.Output);
+            stepperXDir.Write(GpioPinValue.Low);
+
+            stepperXEn = GpioController.GetDefault().OpenPin(18);
+            stepperXEn.SetDriveMode(GpioPinDriveMode.Output);
+            stepperXEn.Write(GpioPinValue.Low);
+
+            // Set Y motor pins
+            stepperYPulse = GpioController.GetDefault().OpenPin(12);
+            stepperYPulse.SetDriveMode(GpioPinDriveMode.Output);
+            stepperYPulse.Write(GpioPinValue.Low);
+
+            stepperYDir = gpio.OpenPin(16);
+            stepperYDir.SetDriveMode(GpioPinDriveMode.Output);
+            stepperYDir.Write(GpioPinValue.Low);
+
+            stepperYEn = GpioController.GetDefault().OpenPin(1);
+            stepperYEn.SetDriveMode(GpioPinDriveMode.Output);
+            stepperYEn.Write(GpioPinValue.Low);
+
+            // Set stepper motor pin
+            servoPulse = GpioController.GetDefault().OpenPin(27);
+            servoPulse.SetDriveMode(GpioPinDriveMode.Output);
+            servoPulse.Write(GpioPinValue.Low);
+
+            // Initialize X-Y pulse timer
+            tmrPulse = new Timer(new TimerCallback(PulseTimerTick), null, 1000, 10);
+        }
+
+        public void SetX(int speed)
+        {
+            if (speed > 0)
+            {
+                stepperXDir.Write(GpioPinValue.High);
+                stepperXEn.Write(GpioPinValue.Low);
+                xMove = true;
+
+            }
+            else if (speed < 0)
+            {
+                stepperXDir.Write(GpioPinValue.Low);
+                stepperXEn.Write(GpioPinValue.Low);
+                xMove = true;
+            }
+            else
+            {
+                stepperXEn.Write(GpioPinValue.High);
+                xMove = false;
+            }
+        }
+
+        public void SetY(int speed)
+        {
+            if (speed > 0)
+            {
+                stepperYDir.Write(GpioPinValue.High);
+                stepperYEn.Write(GpioPinValue.Low);
+                yMove = true;
+            }
+            else if (speed < 0)
+            {
+                stepperYDir.Write(GpioPinValue.Low);
+                stepperYEn.Write(GpioPinValue.Low);
+                yMove = true;
+            }
+            else
+            {
+                stepperYEn.Write(GpioPinValue.High);
+                yMove = false;
+            }
+        }
+
+        private static void PulseTimerTick(object state)
+        {
+            if (xMove)
+                stepperXPulse.Write((stepperXPulse.Read() == GpioPinValue.High ? GpioPinValue.Low : GpioPinValue.High));
+
+            if (yMove)
+                stepperYPulse.Write((stepperYPulse.Read() == GpioPinValue.High ? GpioPinValue.Low : GpioPinValue.High));
+        }
+    }
+}
